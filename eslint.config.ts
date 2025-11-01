e04b9bac912c7bda5ae81a473938f9fdf9bd0cf3
Leonardo Rocha <leonardosrocha@outlook.com>
2025-11-01 13:44:54 +0100

    Add eslint-plugin-jest

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,12 @@
 import js from '@eslint/js'
+import jest from 'eslint-plugin-jest'
 import tseslint from 'typescript-eslint'
 
-export default [js.configs.recommended, ...tseslint.configs.recommended]
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.test.ts'],
+    ...jest.configs['flat/recommended'],
+  },
+]
